1837a0119294ae64a5321510a5c5c37b070b4950
Alvaro Mendes <alvaro.mendes@alunos.unis.edu.br>
2022-03-18 21:21:29 -0300

    POO Atividade.01

---
+++ Produto.php
@@ -0,0 +1,31 @@
+<?php
+class produto{
+
+    public $descricao;
+    public $estoque;
+    public $preco;
+    
+    public function aumentarestoque($valor){
+        $this->estoque += $valor; 
+    }
+    public function diminuirestoque($valor){
+        $this->estoque -= $valor;
+    }
+    public function reajustarpreco($percentual){
+        $this->preco += ($percentual*100)/$this->preco;
+    }
+}
+$produto = new produto;
+$produto->descricao = "produto do Alvaro";
+$produto->estoque = 25;
+$produto->preco = 50;
+
+echo "O $produto->descricao tem $produto->estoque em estoque com o preço unitário de R$$produto->preco,00.";
+
+echo "<br><br>";
+
+$produto->aumentarestoque(10);
+$produto->diminuirestoque(5);
+$produto->reajustarpreco(50);
+
+echo "O $produto->descricao tem $produto->estoque em estoque com o preço unitário de R$$produto->preco,00.";
---
+++ tarefa01.php
@@ -0,0 +1,8 @@
+<?php 
+
+$nome = "Alvaro";  
+$idade = "18";
+     
+echo "Eu sou $nome e tenho $idade anos";
+
+?>
